9da2a5e1f11e7f0de2927bbfe10f2f4a92d094c4
Pratyush Das <reikdas@gmail.com>
2022-08-29 23:36:17 -0400

    List[Tuple] does not work

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,4 @@
-import lms.collection.immutable.{CppCodeGen_List, ListOps}
-import lms.collection.mutable.ArrayOps
+import lms.collection.immutable.{CppCodeGen_List, ListOps, TupleOps}
 import lms.core.stub._
 import lms.macros.SourceContext
 import lms.core.{Backend, virtualize}
@@ -7,7 +6,7 @@ import lms.core.{Backend, virtualize}
 object Main {
   def main(args: Array[String]): Unit = { // Remember to pass string arg
     val arg = args(0)
-    val snippet = new DslDriverCPP[Int, Unit] with ListOps with ArrayOps {
+    val snippet = new DslDriverCPP[Int, Unit] with ListOps with TupleOps {
       q =>
       override val codegen = new DslGenCPP with CppCodeGen_List {
         val IR: q.type = q
@@ -15,20 +14,14 @@ object Main {
 
       @virtualize
       def snippet(dummy: Rep[Int]) = {
-        val arr1 = NewArray[Char](3)
-        val arr2 = NewArray[Char](3)
-        arr1(0) = arg(0)
-        arr2(0) = arg(3)
-        arr1(1) = arg(1)
-        arr2(1) = arg(4)
-        arr1(2) = arg(2)
-        arr2(2) = arg(5)
-        var l: Rep[List[Array[Char]]] = List()
-        l = arr1::l
-        l = arr2::l
+        val foo: Rep[Tuple2[Char, Char]] = Tuple2[Char, Char](arg(0), arg(1))
+        val bar: Rep[Tuple2[Char, Char]] = Tuple2[Char, Char](arg(2), arg(3))
+        var l: Rep[List[Tuple2[Char, Char]]] = List()
+        l = foo::l
+        l = bar::l
         for (i <- 0 until l.size) {
-          for (j <- 0 until l(i).length)
-            println(l(i)(j))
+          println(l(i)._1)
+          println(l(i)._2)
         }
       }
     }
